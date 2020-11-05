b2d715a728bf31aff84729fe9993f17cb8079783
cherina brownlee <cherinabrownlee@amrock.com>
2020-11-05 18:15:39 -0500

    Javascript lab 1

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var name = "Mitch Cuckovich";
+var age = 25;
+var birthday = "January 24";
+var detroitGC = "true";
+detroitGC = true;
+var lifeEvents = ["I was born in Troy, MI", "I went to Hope College", "I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.", "I'm a graduate of the front-end bootcamp."]
+
+if (detroitGC = true) {
+    console.log("My name is Mitch Cuckovich and I am a student at Grand Circus in Detroit, Michigan. I am currently 25 years old and my birthday is on January 24");
+} else {
+    console.log("My name is Bobby Wilkins and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently 21 years old and my birthday is on March 1.");
+}
+
+// write a loop that starts at 0, iterates by increments of 1, while i is less than the length of the lifeEvents array
+
+
+for (i=0; i++; i<4) {
+    console.log(i);
+}
+
+var counter = 0
+
+var randomNumber = 4
